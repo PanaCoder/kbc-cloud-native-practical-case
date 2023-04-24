78f0f7dd2b32386fdb63b3cc6a36391cb77c18b8
Panayot Atanasov <panayot.atanasov@kbc.com>
2023-04-24 11:10:25 +0300

    [lab-03-external-api]

--- ezGroceriesShippingList/src/main/java/com/ezgroceries/shoppinglist/controller/CocktailController.java
+++ ezGroceriesShippingList/src/main/java/com/ezgroceries/shoppinglist/controller/CocktailController.java
@@ -1,6 +1,7 @@
 package com.ezgroceries.shoppinglist.controller;
 
 import com.ezgroceries.shoppinglist.model.Cocktail;
+import com.ezgroceries.shoppinglist.out.CocktailDBClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.UUID;
 
 import static com.ezgroceries.shoppinglist.util.HttpParams.SEARCH;
 
@@ -18,27 +18,11 @@ import static com.ezgroceries.shoppinglist.util.HttpParams.SEARCH;
 @RequiredArgsConstructor
 public class CocktailController {
 
+    private final CocktailDBClient cocktailDBClient;
+
     @GetMapping("/cocktails")
     public ResponseEntity<List<Cocktail>> getCocktails(@RequestParam(value = SEARCH) String search) {
         log.info("--- LOG --- Getting cocktails...");
-        return ResponseEntity.ok().body(getCocktails());
-    }
-
-    private List<Cocktail> getCocktails() {
-        return List.of(
-                new Cocktail()
-                        .setCocktailId(UUID.randomUUID().toString())
-                        .setName("Margerita")
-                        .setGlass("Cocktail glass")
-                        .setInstructions("Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten..")
-                        .setImage("https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg")
-                        .setIngredients(List.of("Tequila", "Triple sec", "Lime juice", "Salt")),
-                new Cocktail()
-                        .setCocktailId(UUID.randomUUID().toString())
-                        .setName("Blue Margerita")
-                        .setGlass("Cocktail glass")
-                        .setInstructions("Rub rim of cocktail glass with lime juice. Dip rim in coarse salt..")
-                        .setImage("https://www.thecocktaildb.com/images/media/drink/qtvvyq1439905913.jpg")
-                        .setIngredients(List.of("Tequila", "Blue Curacao", "Lime juice", "Salt")));
+        return ResponseEntity.ok(cocktailDBClient.searchCocktails(search).toCocktails());
     }
 }
---
+++ ezGroceriesShippingList/src/main/java/com/ezgroceries/shoppinglist/out/CocktailDBResponse.java
@@ -0,0 +1,60 @@
+package com.ezgroceries.shoppinglist.out;
+
+import com.ezgroceries.shoppinglist.model.Cocktail;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CocktailDBResponse {
+
+    @Getter
+    @Setter
+    private List<DrinkResource> drinks = new ArrayList<>();
+
+    public List<Cocktail> toCocktails() {
+        return drinks.stream()
+                .map(DrinkResource::toCocktail)
+                .toList();
+    }
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    public static class DrinkResource {
+        private String idDrink;
+        private String strDrink;
+        private String strGlass;
+        private String strInstructions;
+        private String strDrinkThumb;
+        private String strIngredient1;
+        private String strIngredient2;
+        private String strIngredient3;
+        private String strIngredient4;
+        private String strIngredient5;
+
+        public Cocktail toCocktail() {
+            return new Cocktail()
+                    .setCocktailId(idDrink)
+                    .setName(strDrink)
+                    .setGlass(strGlass)
+                    .setInstructions(strInstructions)
+                    .setImage(strDrinkThumb)
+                    .setIngredients(Stream.of(
+                            strIngredient1,
+                            strIngredient2,
+                            strIngredient3,
+                            strIngredient4,
+                            strIngredient5).filter(StringUtils::isNoneBlank)
+                            .toList());
+        }
+    }
+}
