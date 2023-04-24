69badada290bb0a4884b1374e43970ba57eaf655
Panayot Atanasov <panayot.atanasov@kbc.com>
2023-04-24 11:57:55 +0300

    [lab-04-Persistence-Capability]

--- ezGroceriesShippingList/src/main/java/com/ezgroceries/shoppinglist/model/Cocktail.java
+++ ezGroceriesShippingList/src/main/java/com/ezgroceries/shoppinglist/model/Cocktail.java
@@ -1,17 +1,22 @@
 package com.ezgroceries.shoppinglist.model;
 
 
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Accessors(chain = true)
 @Getter
 @Setter
 public class Cocktail {
+
+    @Id
+    UUID id;
 
     private String cocktailId;
     private String name;
---
+++ ezGroceriesShippingList/src/main/resources/db.migration/V0.1__Add_Cocktail_Table.sql
@@ -0,0 +1,10 @@
+create table COCKTAIL
+(
+    ID           UUID PRIMARY KEY,
+    ID_DRINK     TEXT,
+    NAME         TEXT,
+    GLASS        TEXT,
+    IMAGE        TEXT,
+    INSTRUCTIONS TEXT,
+    INGREDIENTS  TEXT
+)
